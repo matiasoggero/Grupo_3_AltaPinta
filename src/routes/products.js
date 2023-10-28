a88f74f559ee54294407f7f6616eed99c30e2b8f
MeliVandevel <melisavandevelde9289@gmail.com>
2023-10-28 12:35:59 -0300

    Fix products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,7 @@ const productsController = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/productDetail', productsController.productDetail);
-router.get('/productCreation', productsController.productDetail);
-router.get('/productEdition', productsController.productDetail);
+router.get('/productCreation', productsController.productCreation);
+router.get('/productEdition', productsController.productEdition);
 
 module.exports = router;
