5c1ab87dfde22af41f7e2130a0e6f161825aa603
Matias Oggero <matiassoggero@gmail.com>
2023-11-09 10:57:13 -0300

    add delete

--- src/routes/users.js
+++ src/routes/users.js
@@ -23,5 +23,6 @@ router.get('/login', usersController.login);
 //router.post('/login', usersController.login);
 router.get('/register',usersController.register);
 router.post('/register',upload.single('avatar'),usersController.create);
+router.delete('???',usersController.delete);
 
 module.exports = router;
