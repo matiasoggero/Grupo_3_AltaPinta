5e3cb3215724485c00c8673a4da1615a477f6e75
MeliVandevel <melisavandevelde9289@gmail.com>
2023-11-14 21:19:17 -0300

    fix product.json  property image and fix about creation product

--- src/routes/products.js
+++ src/routes/products.js
@@ -25,7 +25,7 @@ const router = express.Router();
 router.get("/productDetail", productsController.productDetail);
 router.get("/products", productsController.productsShow);
 router.get("/productCreation", productsController.productCreation);
-router.post("/productCreation",upload.single("image") ,productsController.productStore);
+router.post("/productCreation",upload.single("image"), productsController.productStore);
 router.get("/productEdition", productsController.productEdition);
 router.get("/detailOne/:id", productsController.detail);
 router.delete("/:id/delete", productsController.destroy);
