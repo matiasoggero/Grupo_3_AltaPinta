24429df4d106345c692a03b20aa901b7e7f8e56d
German Poupeau <poupeaug@gmail.com>
2023-11-16 10:21:45 -0300

    require express validator and create validator form in routes/users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const multer = require('multer');
 const path = require('path');
+const {body} = require('express-validator');
 
 const usersController = require('../controllers/usersController');
 
@@ -15,7 +16,15 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage})
+const validatorForm = [
+    body('name').notEmpty().withMessage("Debes completar este campo"),
+    body('email').isEmail().withMessage("Debes completar este campo"),
+    body('date').notEmpty().withMessage("Debes completar este campo"),
+    body('address').notEmpty().withMessage("Debes completar este campo"),
+    body('phone').notEmpty().withMessage("Debes completar este campo")
+];
+
+const upload = multer({storage});
 
 const router = express.Router();
 
@@ -23,6 +32,7 @@ router.get('/login', usersController.login);
 //router.post('/login', usersController.login);
 router.get('/register',usersController.register);
 router.post('/register',upload.single('avatar'),usersController.create);
+router.post('/register',validatorForm,usersController.create);//ver esta línea
 router.delete('/:id/delete',usersController.delete);
 router.get('/admin',usersController.admin);
 router.get('/', usersController.list);
