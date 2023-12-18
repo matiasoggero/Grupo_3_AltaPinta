a89ad41607b56be3a8367eca91fd9506caf18467
rocio salamano <rociosalamano@gmail.com>
2023-12-18 16:40:13 -0300

    remove console log

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,8 @@
 function protectRoute(req, res, next) {
-  console.log(req.session);
   if (!req.session.user) {
     return res.redirect("/users/login"); // Redirigir al login si el usuario no está autenticado
   }
   next(); // Continuar si el usuario está autenticado
 }
-  
-  module.exports = protectRoute;
+
+module.exports = protectRoute;
